e7a17b7c92fd5a9c84f97446286f02e33e2f2f08
Rui <958114081@qq.com>
2016-03-18 21:21:04 +0800

    注意vote模块最后一句空格出错

--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
@@ -22,7 +22,7 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question':question})
 
 
-def vote(request,question_id):
+def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
@@ -34,5 +34,23 @@ def vote(request,question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls: results', args=(p.id,)))
+        # return HttpResponseRedirect(reverse('polls: results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
+# def vote(request, question_id):
+#     p = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = p.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the question voting form.
+#         return render(request, 'polls/detail.html', {
+#             'question': p,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
